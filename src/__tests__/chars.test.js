d1b13ab523e3aa4471bfd16b896c940b9b178f15
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-05-06 11:02:03 +0300

    Test cases modified

--- src/__tests__/chars.test.js
+++ src/__tests__/chars.test.js
@@ -2,8 +2,14 @@ import { chars, sorting, healthCheck } from '../index'
 
 healthCheck()
 
-test('Check', () => {
+test('The healthiest guy', () => {
     expect(chars[0].health).toEqual(100);
-    expect(chars[1].health).toEqual(80);
-    expect(chars[2].health).toEqual(10);
+})
+
+test('Just a scratch', () => {
+expect(chars[1].health).toEqual(80);
+})
+
+test('Call a doctor!', () =>{
+expect(chars[2].health).toEqual(10);
 })
