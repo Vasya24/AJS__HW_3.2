8bcf3a7c7bca849ea76af3fefd952271769e41eb
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-05-07 18:19:04 +0300

    Code has been fully reconcepted and now it works perfect

--- src/__tests__/chars.test.js
+++ src/__tests__/chars.test.js
@@ -1,15 +1,13 @@
-import { chars, sorting, healthCheck } from '../index'
-
-healthCheck()
+import { chars, sorting } from '../index'
 
 test('The healthiest guy', () => {
     expect(chars[0].health).toEqual(100);
 })
 
 test('Just a scratch', () => {
-expect(chars[1].health).toEqual(80);
+expect(chars[1].health).toBe(80);
 })
 
 test('Call a doctor!', () =>{
-expect(chars[2].health).toEqual(10);
+expect(chars[2].health).toBe(10);
 })
--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ let sorting = chars.sort(function(a, b) {
    return b.health - a.health
 })
 
-function healthCheck() {
-for (let i=0; i<chars.length; i++) {
-    if (chars[i].health == 100) {
-        console.log('The healthiest guy')
-    };
-    if (chars[i].health == 80) {
-        console.log('Just a scratch')
-    };
-    if (chars[i].health == 10) {
-        console.log('Call a doctor!')
-    }
-}
-}
-export { chars, sorting, healthCheck }
+// function healthCheck(chars) {
+// for (let i=0; i<chars.length; i++) {
+//     if (chars[i].health == 100) {
+//         console.log('The healthiest guy')
+//     };
+//     if (chars[i].health == 80) {
+//         console.log('Just a scratch')
+//     };
+//     if (chars[i].health == 10) {
+//         console.log('Call a doctor!')
+//     }
+// }
+// }
+export { chars, sorting }
